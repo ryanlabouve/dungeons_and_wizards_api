bc37e1c876aef1135b8bd2b0430510eace6f39be
Ryan LaBouve <imryanlabouve@gmail.com>
2017-06-04 18:21:14 -0500

    Primitive battle moves

--- app/controllers/battle_controller.rb
+++ app/controllers/battle_controller.rb
@@ -3,4 +3,22 @@ class BattleController < ApplicationController
     @battle = Battle.last || Battle.create!({ hp_total: 1000, damage_total: 0 })
     render json: @battle
   end
+
+  def move
+    # TODO refactor this to be less dumb
+    @battle = Battle.last
+    @move_name = params['name']
+    @player = Player.where('moves @> ?', "[{\"name\": \"#{@move_name}\"}]")
+    # TODO error if no player
+    @move = @player.first.moves.select { |m| m['name'] == @move_name }
+    @move = @move.first
+    sleep @move["queue_time"].to_i
+    @battle.damage_total += @move["damage"].to_i
+    if @battle.save!
+      render json: { "success": true }
+    else
+      render json: { "success": false }
+    end
+    # TODO Push to activity log
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :players
   get "/battle", to: 'battle#stats'
+  post "/move", to: 'battle#move'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
