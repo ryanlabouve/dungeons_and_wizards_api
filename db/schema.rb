1311a72279787186123eca24457e83857f29770a
Ryan LaBouve <imryanlabouve@gmail.com>
2017-06-07 00:34:19 -0500

    Handle risky moves

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170606023318) do
+ActiveRecord::Schema.define(version: 20170607044820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170606023318) do
     t.string   "activity_by"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.boolean  "successful"
   end
 
   create_table "battles", force: :cascade do |t|
